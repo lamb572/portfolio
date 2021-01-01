bd8a4fa02d8ded00e2c77400395d3bf1343513fa
lamb572 <55807998+lamb572@users.noreply.github.com>
2021-01-01 18:44:58 +0000

    update to about me

--- src/components/about-me/AboutMe.js
+++ src/components/about-me/AboutMe.js
@@ -1,15 +1,20 @@
 import React from 'react';
 
 function AboutMe({id}) {
+    const myInfo = "After a career in the British Army, I am now using knowledge learned through extra studies, to increase my experience and build up this portfolio to move into a new career as a Frontend Web Developer. \nMy current skills come from online class’s, most recently I have studied using the course 'The Complete Web Developer in 2021: Zero to Mastery' by Andrei Neagoie. \nMy current focus is to increase my knowledge and experience by building front end web pages using React. \nI also have a basic understanding in backend development using NodeJS and writing scripts in Python, some examples are held within my GitHub repositories. My LinkedIn page contains some more information on qualifications and experiences I hold. \nPlease see below the current live projects I’ve built"
+
+
+    let newInfo = myInfo.split('\n').map(i => {
+        return <p>{i}</p>
+      });
+
+
     return (
         <div className="section" id={id} >
             <h1>About Me</h1>
             <div className="Am">
                 <p>
-                    Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                    Excepturi neque, ipsa animi maiores repellendu distinctioaperiam
-                    earum dolor voluptatum consequatur blanditiis inventore debitis
-                    fuga numquam voluptate architecto itaque molestiae.
+                {newInfo}
                 </p>
             </div>
             
--- src/components/contact/contact.js
+++ src/components/contact/contact.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import './contact.css';
 
 function Contact({id}) {
     return (
         <div className="section" id={id}>
+            <p>
+                If you wish to contact me please use my  
+                 <a className="intextlink" target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/lukemb/" title="LinkedIn"> LinkedIn </a>
+                 account.
+            </p>
             
         </div>
     );
